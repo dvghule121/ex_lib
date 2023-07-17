7033134e41a925e8ed0288b49f1c97b9cf4010f0
Dyno <dvghule121@gmail.com>
2023-07-18 00:32:24 +0530

    some changes

--- ex_v1/src/main/java/com/dynocodes/ex_v1/Toaster.kt
+++ ex_v1/src/main/java/com/dynocodes/ex_v1/Toaster.kt
@@ -1,4 +1,10 @@
 package com.dynocodes.ex_v1
 
+import android.content.Context
+import android.widget.Toast
+
 class Toaster {
+    fun toast(context: Context){
+        Toast.makeText(context, "hi", Toast.LENGTH_SHORT)
+    }
 }
